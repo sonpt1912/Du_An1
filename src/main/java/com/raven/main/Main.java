984f8598782faf5e73de95e99d6e6bdc96e076ce
tunld23 <buicongtuan1478@gmail.com>
2022-11-25 00:00:39 +0700

    Header hơi sida nên tôi sửa lại nhé

--- src/main/java/com/raven/main/Main.java
+++ src/main/java/com/raven/main/Main.java
@@ -6,6 +6,7 @@
 package com.raven.main;
 
 import com.mycompany.domainModel.NhanVien;
+import com.raven.component.ClockThread;
 import com.raven.event.EventMenuSelected;
 import com.raven.form.Form_Ban;
 import com.raven.form.Form_Combo;
@@ -43,7 +44,7 @@ public class Main extends javax.swing.JFrame {
 
     public Main(NhanVien nv) {
         initComponents();
-        jLabel2.setText(nv.getMa());
+        lbTenMaNhanVien.setText(nv.getMa() + " - " + nv.getChucVu().getTen() + " - " + nv.getHo() + " " + nv.getTenDem() + " " + nv.getTen());
         setBackground(new Color(0, 0, 0, 0));
         home = new Form_Home();
         form1 = new Form_HoaDon();
@@ -117,8 +118,9 @@ public class Main extends javax.swing.JFrame {
         menu = new com.raven.component.Menu();
         mainPanel = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        lbTenMaNhanVien = new javax.swing.JLabel();
+        lbClock = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -126,9 +128,27 @@ public class Main extends javax.swing.JFrame {
         mainPanel.setOpaque(false);
         mainPanel.setLayout(new java.awt.BorderLayout());
 
-        jLabel1.setText("jLabel1");
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        jLabel2.setText("jLabel2");
+        lbTenMaNhanVien.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        lbTenMaNhanVien.setText("Nguyễn Đức Dụng - Nhân Viên");
+
+        lbClock.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        lbClock.setText("2022-11-17 12:42:20");
+        lbClock.addAncestorListener(new javax.swing.event.AncestorListener() {
+            public void ancestorAdded(javax.swing.event.AncestorEvent evt) {
+                lbClockAncestorAdded(evt);
+            }
+            public void ancestorMoved(javax.swing.event.AncestorEvent evt) {
+            }
+            public void ancestorRemoved(javax.swing.event.AncestorEvent evt) {
+            }
+        });
+
+        jButton1.setBackground(new java.awt.Color(0, 0, 0));
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Giao Ca");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -136,19 +156,22 @@ public class Main extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(24, 24, 24)
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(lbTenMaNhanVien)
+                .addGap(30, 30, 30)
+                .addComponent(lbClock)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(21, 21, 21))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addGap(22, 22, 22))
+                    .addComponent(lbClock)
+                    .addComponent(jButton1)
+                    .addComponent(lbTenMaNhanVien))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
@@ -171,7 +194,7 @@ public class Main extends javax.swing.JFrame {
             .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 657, Short.MAX_VALUE)
             .addGroup(panelBorder1Layout.createSequentialGroup()
                 .addGap(5, 5, 5)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
@@ -191,6 +214,12 @@ public class Main extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void lbClockAncestorAdded(javax.swing.event.AncestorEvent evt) {//GEN-FIRST:event_lbClockAncestorAdded
+        // TODO add your handling code here:
+        ClockThread clock = new ClockThread(lbClock);
+        clock.start();
+    }//GEN-LAST:event_lbClockAncestorAdded
 
     /**
      * @param args the command line arguments
@@ -228,9 +257,10 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lbClock;
+    private javax.swing.JLabel lbTenMaNhanVien;
     private javax.swing.JPanel mainPanel;
     private com.raven.component.Menu menu;
     private com.raven.swing.PanelBorder panelBorder1;
