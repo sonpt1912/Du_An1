c4dbf1be602f111c379aadd5150c94a9ee605b2b
Sonpt <phamson191203@gmail.com>
2022-12-10 00:21:05 +0700

    past 2

--- target/classes/com/raven/main/Main.java
+++ target/classes/com/raven/main/Main.java
@@ -105,13 +105,13 @@ public class Main extends javax.swing.JFrame {
                     } else if (index == 4) {
                         setForm(form4);
                     } else if (index == 5) {
-                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền khách hàng");
+                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền quản lý");
                     } else if (index == 6) {
-                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền khách hàng");
+                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền quản lý");
                     } else if (index == 7) {
-                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền khách hàng");
+                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền quản lý");
                     } else if (index == 8) {
-                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền khách hàng");
+                        JOptionPane.showMessageDialog(rootPane, "vui lòng sử dụng quyền quản lý");
                     } else if (index == 9) {
                         setForm(form9);
                     } else if (index == 10) {
