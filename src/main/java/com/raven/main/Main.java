07a56201f7fd98d2b82dd73d00452c295e4a370e
thanhnvph20218 <thanhnvph20219@fpt.edu.vn>
2022-12-11 23:03:21 +0700

    Main ne

--- src/main/java/com/raven/main/Main.java
+++ src/main/java/com/raven/main/Main.java
@@ -155,7 +155,8 @@ public class Main extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         lbTenMaNhanVien = new javax.swing.JLabel();
         lbClock = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnLogout = new javax.swing.JButton();
+        btnThuNho = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -180,10 +181,25 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setBackground(new java.awt.Color(0, 0, 0));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Giao Ca");
+        btnLogout.setBackground(new java.awt.Color(204, 204, 255));
+        btnLogout.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnLogout.setForeground(new java.awt.Color(255, 0, 51));
+        btnLogout.setText("X");
+        btnLogout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLogoutActionPerformed(evt);
+            }
+        });
+
+        btnThuNho.setBackground(new java.awt.Color(204, 204, 255));
+        btnThuNho.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnThuNho.setForeground(new java.awt.Color(255, 51, 51));
+        btnThuNho.setText("-");
+        btnThuNho.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnThuNhoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -195,8 +211,10 @@ public class Main extends javax.swing.JFrame {
                 .addGap(30, 30, 30)
                 .addComponent(lbClock)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(21, 21, 21))
+                .addComponent(btnThuNho)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnLogout)
+                .addGap(18, 18, 18))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -204,9 +222,10 @@ public class Main extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbClock)
-                    .addComponent(jButton1)
-                    .addComponent(lbTenMaNhanVien))
-                .addContainerGap(17, Short.MAX_VALUE))
+                    .addComponent(lbTenMaNhanVien)
+                    .addComponent(btnLogout)
+                    .addComponent(btnThuNho))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
@@ -256,6 +275,25 @@ public class Main extends javax.swing.JFrame {
         clock.start();
     }//GEN-LAST:event_lbClockAncestorAdded
 
+    private void btnLogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLogoutActionPerformed
+        // TODO add your handling code here:
+        int checkOut = JOptionPane.showConfirmDialog(null, "Bạn có muốn đăng xuất không");
+        if (checkOut == JOptionPane.NO_OPTION) {
+            return;
+        } else if (checkOut == JOptionPane.CLOSED_OPTION) {
+            return;
+        } else if (checkOut == JOptionPane.CANCEL_OPTION) {
+            return;
+        } else {
+            System.exit(0);
+        }
+    }//GEN-LAST:event_btnLogoutActionPerformed
+
+    private void btnThuNhoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThuNhoActionPerformed
+        // TODO add your handling code here:
+        this.setExtendedState(Main.ICONIFIED);
+    }//GEN-LAST:event_btnThuNhoActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -300,7 +338,8 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnLogout;
+    private javax.swing.JButton btnThuNho;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lbClock;
     private javax.swing.JLabel lbTenMaNhanVien;
